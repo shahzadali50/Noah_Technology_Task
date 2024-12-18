f65ec8d0202d321e6c24a5c2339e0c829eca2f23
Shahzad Ali <108119561+shahzadali50@users.noreply.github.com>
2024-12-19 03:19:50 +0500

    customer_slider

---
+++ assets/javascript/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('.product_slider .owl-carousel').owlCarousel({
+        loop:true,
+        margin:9,
+        nav:false,
+        autoplay: true,         // Enables autoplay
+        autoplayTimeout: 2000,  // Delay between transitions (in milliseconds)
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:2
+            },
+            576:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            992:{
+                items:4
+            },
+            1200:{
+                items:5
+            }
+        }
+    })
+  
+})
