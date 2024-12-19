77452587934d6620d63c974a1fed960a8a0b96f1
Shahzad Ali <108119561+shahzadali50@users.noreply.github.com>
2024-12-19 12:41:51 +0500

    Hero section done

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -32,11 +32,31 @@ $(document).ready(function(){
         margin:9,
         nav:false,
         autoplay: true,         // Enables autoplay
-        autoplayTimeout: 2000,  // Delay between transitions (in milliseconds)
+        autoplayTimeout: 3000,  // Delay between transitions (in milliseconds)
         autoplayHoverPause: true,
         responsive:{
             0:{
                 items:1
+            }
+        }
+    })
+  
+})
+$(document).ready(function(){
+    $('.client_logos .owl-carousel').owlCarousel({
+        loop:true,
+        margin:9,
+        nav:false,
+        autoplay: true,  
+        autoplayTimeout: 0,     // Continuous movement without delay
+        autoplaySpeed: 6000,   // Speed of the transition (in milliseconds)
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:5
+            },
+            768:{
+                items:12
             }
         }
     })
